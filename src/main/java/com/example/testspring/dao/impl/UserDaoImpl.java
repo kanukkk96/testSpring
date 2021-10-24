e534638908ca010708177fb3ebb997dc967b580e
kanuk <kanukkk96@naver.com>
2021-10-24 20:42:04 +0900

    리스트 객체 수정

--- src/main/java/com/example/testspring/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/testspring/dao/impl/UserDaoImpl.java
@@ -13,10 +13,28 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getUserById(Integer id) {
+        User u1 = new User();
+        u1.username = "kanuk";
+        u1.age = 27;
+
+        User u2 = new User();
+        u2.username = "geonwoo";
+        u2.age = 22;
+
+        User u3 = new User();
+        u3.username = "kanu";
+        u3.age = 26;
+
+        User u4 = new User();
+        u4.username = "capp";
+        u4.age = 14;
         List<User> userList = new ArrayList<User>();
 
-        userList.add(new User("c",14));
-        userList.add(new User("kanu",26));
+        userList.add(u1);
+        userList.add(u2);
+        userList.add(u3);
+        userList.add(u4);
+
         return userList;
     }
 
